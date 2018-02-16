0933d0bdb099741961b57a4890c9a4cf12aa975a
Laurent Bossavit <laurent.git@bossavit.com>
2018-02-16 08:52:47 +0100

    Use request_last_or_next for contrat de travail

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/allegements.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/allegements.py
@@ -52,8 +52,8 @@ class coefficient_proratisation(Variable):
         #   forfait_heures_annee
         #   forfait_jours_annee ]
 
-        contrat_de_travail_debut = simulation.calculate('contrat_de_travail_debut', '3999-01')
-        contrat_de_travail_fin = simulation.calculate('contrat_de_travail_fin', '3999-01')
+        contrat_de_travail_debut = simulation.calculate('contrat_de_travail_debut', period)
+        contrat_de_travail_fin = simulation.calculate('contrat_de_travail_fin', period)
 
         # Volume des heures rémunérées à un forfait heures
         forfait_heures_remunerees_volume = simulation.calculate('forfait_heures_remunerees_volume', period)
