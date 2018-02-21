13bd179a3e30e0a89e642053b72a87f16de49cc3
Claire Leroy <cleroy@pse.local>
2018-02-21 10:41:08 +0100

    Clean and simplify code

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot.py
@@ -3854,29 +3854,27 @@ class saldom(Variable):
 
     def formula_2002_01_01(foyer_fiscal, period, parameters):
         '''
-        Sommes versées pour l'emploi d'un salariés à  domicile
+        Sommes versées pour l'emploi d'un salariés à domicile
         2002-2004
         '''
         f7df = foyer_fiscal('f7df', period)
-        f7dg = foyer_fiscal('f7dg', period)
+        isinvalid = foyer_fiscal('f7dg', period)
         P = parameters(period).impot_revenu.reductions_impots.salarie_domicile
 
-        isinvalid = f7dg
         max1 = P.max1 * not_(isinvalid) + P.max3 * isinvalid
         return P.taux * min_(f7df, max1)
 
     def formula_2005_01_01(foyer_fiscal, period, parameters):
         '''
-        Sommes versées pour l'emploi d'un salariés à  domicile
+        Sommes versées pour l'emploi d'un salariés à domicile
         2005-2006
         '''
         nb_pac2 = foyer_fiscal('nb_pac2', period)
         f7df = foyer_fiscal('f7df', period)
         f7dl = foyer_fiscal('f7dl', period)
-        f7dg = foyer_fiscal('f7dg', period)
+        isinvalid = foyer_fiscal('f7dg', period)
         P = parameters(period).impot_revenu.reductions_impots.salarie_domicile
 
-        isinvalid = f7dg
         nbpacmin = nb_pac2 + f7dl
         maxBase = P.max1
         maxDuMaxNonInv = P.max2
@@ -3886,17 +3884,16 @@ class saldom(Variable):
 
     def formula_2007_01_01(foyer_fiscal, period, parameters):
         '''
-        Sommes versées pour l'emploi d'un salariés à  domicile (à partir de 2007, 7DB donne droit à un crédit et 7DF à une réduction)
+        Sommes versées pour l'emploi d'un salariés à domicile (à partir de 2007, 7DB donne droit à un crédit et 7DF à une réduction)
         2007-2008
         '''
         nb_pac2 = foyer_fiscal('nb_pac2', period)
         f7db = foyer_fiscal('f7db', period)
         f7df = foyer_fiscal('f7df', period)
         f7dl = foyer_fiscal('f7dl', period)
-        f7dg = foyer_fiscal('f7dg', period)
+        isinvalid = foyer_fiscal('f7dg', period)
         P = parameters(period).impot_revenu.reductions_impots.salarie_domicile
 
-        isinvalid = f7dg
         nbpacmin = nb_pac2 + f7dl
         maxBase = P.max1
         maxDuMaxNonInv = P.max2
@@ -3907,19 +3904,17 @@ class saldom(Variable):
 
     def formula_2009_01_01(foyer_fiscal, period, parameters):
         '''
-        Sommes versées pour l'emploi d'un salariés à  domicile
+        Sommes versées pour l'emploi d'un salariés à domicile
         2009-2011
         '''
         nb_pac2 = foyer_fiscal('nb_pac2', period)
         f7db = foyer_fiscal('f7db', period)
         f7df = foyer_fiscal('f7df', period)
         f7dl = foyer_fiscal('f7dl', period)
-        f7dq = foyer_fiscal('f7dq', period)
-        f7dg = foyer_fiscal('f7dg', period)
+        annee1 = foyer_fiscal('f7dq', period)
+        isinvalid = foyer_fiscal('f7dg', period)
         P = parameters(period).impot_revenu.reductions_impots.salarie_domicile
 
-        isinvalid = f7dg
-        annee1 = f7dq
         nbpacmin = nb_pac2 + f7dl
         maxBase = P.max1 * not_(annee1) + P.max1_1ereAnnee * annee1
         maxDuMaxNonInv = P.max2 * not_(annee1) + P.max2_1ereAnnee * annee1
@@ -3932,7 +3927,7 @@ class saldom(Variable):
 
     def formula_2011_01_01(self, simulation, period):
         '''
-        Sommes versées pour l'emploi d'un salariés à  domicile
+        Sommes versées pour l'emploi d'un salariés à domicile
         2011 -
         '''
         nb_pac2 = foyer_fiscal('nb_pac2', period)
@@ -3940,12 +3935,10 @@ class saldom(Variable):
         f7dd = foyer_fiscal('f7dd', period)
         f7df = foyer_fiscal('f7df', period)
         f7dl = foyer_fiscal('f7dl', period)
-        f7dq = foyer_fiscal('f7dq', period)
-        f7dg = foyer_fiscal('f7dg', period)
+        annee1 = foyer_fiscal('f7dq', period)
+        isinvalid = foyer_fiscal('f7dg', period)
         P = parameters(period).impot_revenu.reductions_impots.salarie_domicile
 
-        isinvalid = f7dg
-        annee1 = f7dq
         nbpacmin = nb_pac2 + f7dl
         maxBase = P.max1 * not_(annee1) + P.max1_1ereAnnee * annee1
         maxDuMaxNonInv = P.max2 * not_(annee1) + P.max2_1ereAnnee * annee1
